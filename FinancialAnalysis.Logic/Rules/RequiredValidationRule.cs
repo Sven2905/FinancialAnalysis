3116f214abb73bfd12d5dc7f0b2718a4855fff67
Markus <mighty.panders@gmail.com>
2019-02-10 17:02:52 +0100

    Inputvalidation for EmployeeControl. Employee validates itself via Property

---
+++ FinancialAnalysis.Logic/Rules/RequiredValidationRule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace FinancialAnalysis.Logic.Rules
+{
+    public class RequiredValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string error = GetErrorMessage(FieldName, value);
+            if (!string.IsNullOrEmpty(error))
+                return new ValidationResult(false, error);
+            return ValidationResult.ValidResult;
+        }
+        public string FieldName { get; set; }
+        public static string GetErrorMessage(string fieldName, object fieldValue, object nullValue = null)
+        {
+            string errorMessage = string.Empty;
+            if (nullValue != null && nullValue.Equals(fieldValue))
+                errorMessage = string.Format($"You cannot leave Field {fieldName} empty");
+            if (fieldValue == null || string.IsNullOrEmpty(fieldValue?.ToString()))
+                errorMessage = string.Format($"You cannot leave Field {fieldName} empty");
+            return errorMessage;
+        }
+    }
+}
