beaf0b81692bb5a86f1ef08697c95be1304b0da4
Sven Fuhrmann <sven.fuhrmann@asinco.de>
2019-05-06 10:50:26 +0200

    Added Depreciation

---
+++ FinancialAnalysis.Logic/ViewModels/Accounting/DepreciationViewModel.cs
@@ -0,0 +1,75 @@
+﻿using DevExpress.Mvvm;
+using FinancialAnalysis.Models;
+using Formulas.DepreciationMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace FinancialAnalysis.Logic.ViewModels
+{
+    public class DepreciationViewModel : ViewModelBase
+    {
+        public DepreciationViewModel()
+        {
+            CalculateCommand = new DelegateCommand(Calculate);
+        }
+
+        private DepreciationType _DepreciationType;
+        private int _Years;
+        private decimal _InitialValue;
+        private decimal _AssetValue;
+
+        public DepreciationType DepreciationType
+        {
+            get { return _DepreciationType; }
+            set { _DepreciationType = value; Calculate(); }
+        }
+
+        public int Years
+        {
+            get { return _Years; }
+            set { _Years = value; Calculate(); }
+        }
+
+        public decimal InitialValue
+        {
+            get { return _InitialValue; }
+            set { _InitialValue = value; Calculate(); }
+        }
+
+        public decimal AssetValue
+        {
+            get { return _AssetValue; }
+            set { _AssetValue = value; Calculate(); }
+        }
+
+        public SvenTechCollection<DepreciationValue> DepreciationValues { get; set; }
+        public DelegateCommand CalculateCommand { get; set; }
+
+        private void Calculate()
+        {
+            if (Years <= 0 || InitialValue <= 0)
+            {
+                return;
+            }
+
+            switch (DepreciationType)
+            {
+                case DepreciationType.ArithmenticDegressive:
+                    DepreciationValues = Depreciations.CalculateArithmenticDegressiveValuesForYears(InitialValue, AssetValue, Years).ToSvenTechCollection();
+                    break;
+                case DepreciationType.GeometryDregressive:
+                    DepreciationValues = Depreciations.CalculateGeometryDregressiveForYears(InitialValue, AssetValue, Years).ToSvenTechCollection();
+                    break;
+                case DepreciationType.Linear:
+                    DepreciationValues = Depreciations.CalculateLinearValueForYears(InitialValue, AssetValue, Years).ToSvenTechCollection();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ FinancialAnalysis.Models/Enums/DepreciationType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialAnalysis.Models
+{
+    public enum DepreciationType
+    {
+        [Display(Name = "Arithmetisch-degressiv")] ArithmenticDegressive,
+        [Display(Name = "Geometrisch-degressiv")] GeometryDregressive,
+        [Display(Name = "Linear")] Linear
+    }
+}
